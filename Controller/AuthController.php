091d897476ac8d4f03abb749cfb1434394f41fad
PRINCE <princenagac12@gmail.com>
2024-08-27 00:12:21 +0800

    First try for this project 1

---
+++ Controller/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+class AuthController
+{
+
+    public function index(){
+        
+    }
+
+
+    public function register($id)
+    {
+
+    }
+
+    public function login($email, $password)
+    {
+          
+    }
+
+
+}
+
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "hello";
+?>
+
+<div>
+I'm going to do the Localhost Web Server in this command php -S localhost:8080 command.
+</div>
