9a3f86fed419682cf4db2977e52ee3d563359bf0
PRINCE <princenagac12@gmail.com>
2024-08-31 03:06:46 +0800

    Added Task Table Migration

--- app/migrations/create_task_table.php
+++ app/migrations/create_task_table.php
@@ -5,22 +5,22 @@ $dbname = "mvc_app";
 $user = "root";
 $pass = "";
 
-try{
+try {
 
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user , $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "CREATE TABLE tasks(id INT AUTO_INCREMENT PRIMARY KEY, 
-                               task_data varchar(255) NOT NULL,
-                               status ENUM('Not-Completed', 'Completed') DEFAULT 'Not-Completed', 
-                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                               user_id INT NOT NULL FOREIGN KEY REFERENCES users(id),
-                               )";
- 
+    $sql = "CREATE TABLE tasks (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        task_data VARCHAR(255) NOT NULL,
+                        status ENUM('Not-Completed', 'Completed') DEFAULT 'Not-Completed',
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                        user_id INT NOT NULL,
+                        FOREIGN KEY (user_id) REFERENCES users(id)
+                              )";
+
     $pdo->exec($sql);
-    echo "Task Table Created Successfully";    
-}
-catch(Exception $e)
-{
-    echo "Error: ". $e->getMessage();
+    echo "Task Table Created Successfully";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
 }
