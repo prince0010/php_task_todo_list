48caa8ce339a895d3941c2b6785d210006c447f5
PRINCE <princenagac12@gmail.com>
2024-08-31 23:00:34 +0800

    Adding Logic in Register in AuthController and the logic to communicate to the db of a table in the User Model

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -8,11 +8,21 @@ class AuthController
     }
 
 
-    public function register($id)
+    public function register()
     {
+        $id = $_POST['id'];
+        $firstname = $_POST['firstname'];
+        $middlename = $_POST['middlename'];
+        $lastname = $_POST['lastname'];
+        $address = $_POST['address'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
+        $user = new User();
+
+        // $user->register($id, $firstname, $middlename, $lastname, $address, $phone, $email, $password);
         
-
     }
 
     public function login($email, $password)
