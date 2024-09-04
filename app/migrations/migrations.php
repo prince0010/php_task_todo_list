cb39cdfb39bd9f75c99fac7e985996a14fef39e6
PRINCE <princenagac12@gmail.com>
2024-09-04 22:36:13 +0800

    fix the errors in the register in the AuthController and some logics in the User.php and in the register.view.php fixed the location in the ajax for url and in the method and in the Index.php I added the  variable for the ['action'] and adding a if for the register but if there is a login na then you can use the switch statement that is being commented out in the index.php

--- app/migrations/migrations.php
+++ app/migrations/migrations.php
@@ -11,24 +11,24 @@
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo "Database connection established.<br>";
+        // echo "Database connection established.<br>";
 
         // Include the Migration Files
         include '2024_08_31_create_user_table.php';
         include '2024-08-31_create_task_table.php';
 
-        echo "Starting migrations: <br>";
+        // echo "Starting migrations: <br>";
 
-        // Execute the Migration functions
-        echo "Running user table migration...<br>";
+        // // Execute the Migration functions
+        // echo "Running user table migration...<br>";
         createUserTable($pdo);
-        echo "<br>";
+        // echo "<br>";
 
-        echo "Running task table migration...<br>";
+        // echo "Running task table migration...<br>";
         createTaskTable($pdo);
-        echo "<br>";
+        // echo "<br>";
 
-        echo "All migrations executed successfully!<br>";
+        // echo "All migrations executed successfully!<br>";
     }
     catch(Exception $e)
     {
